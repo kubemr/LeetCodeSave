df8ed76b3b79c27299829e82b6c96a2e3dfc92be
NotSpir <micheilkubrakin@gmail.com>
2026-05-26 11:08:11 +0300

    y

--- LeetCodeSave/Program.cs
+++ LeetCodeSave/Program.cs
@@ -6,26 +6,6 @@ using LeetCodeSave.SlidingWindow;
 using LeetCodeSave.Stack;
 using LeetCodeSave.TwoPointers;
 
-
-
-
-
-
-
-Console.WriteLine(DecodeString.ExecuteSolution("3[z]2[2[y]pq4[2[jk]e1[f]]]ef")); //"zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef"
-Console.WriteLine(DecodeString.ExecuteSolution("3[a]2[bc]")); //"aaabcbc"
-Console.WriteLine(DecodeString.ExecuteSolution("3[a2[c]]")); //"accaccacc"
-Console.WriteLine(DecodeString.ExecuteSolution("2[abc]3[cd]ef")); //"abcabccdcdcdef"
-
-//Console.WriteLine(DecodeString.ExecuteSolution("3[a]2[bc]"));
-//Console.WriteLine(DecodeString.ExecuteSolution("3[a2[c]]"));
-//Console.WriteLine(DecodeString.ExecuteSolution("2[abc]3[cd]ef"));
-
-//NumberOfRecentCalls.ExecuteSolution();
-
-//Console.WriteLine(Dota2Senate.ExecuteSolution("RD"));
-//Console.WriteLine(Dota2Senate.ExecuteSolution("RDD"));
-//Console.WriteLine(Dota2Senate.ExecuteSolution("RDDRDRDRRDR"));
-//Console.WriteLine(Dota2Senate.ExecuteSolution("RRDRDRDRDRDRDRRDRDRDRDRDRRDRDRDD"));
+//Tests
 
 Console.ReadLine();
--- LeetCodeSave/obj/Debug/net8.0/LeetCodeSave.AssemblyInfo.cs
+++ LeetCodeSave/obj/Debug/net8.0/LeetCodeSave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCodeSave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81460a4febf821ef59dc5a4dbb084632a2c27f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCodeSave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCodeSave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
